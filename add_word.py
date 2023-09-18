55adc649dbf728902323b3fc1aaa59782f6ffd69
raquellewei <raquelle70679@gmail.com>
2023-09-18 08:29:16 -0400

    further modificatino

--- add_word.py
+++ add_word.py
@@ -1,12 +1,14 @@
 from tkinter import *
-from PIL import ImageTk
+from PIL import ImageTk, Image
+import json
 
 
-BACKGROUND_COLOR = "#FBF0B2"
+#BACKGROUND_COLOR = "#FBF0B2"
 PINK = '#FFC7EA'
 BLUE = '#CAEDFF'
 PURPLE = '#D8B4F8'
 FONT = 'Comic Sans MS'
+BACKGROUND_COLOR = BLUE
 
 # UI Setup
 window = Tk()
@@ -24,10 +26,21 @@ translation_box = Entry(highlightthickness=0, highlightbackground=BACKGROUND_COL
 translation_box.grid(row=2, column=1)
 add_button = Button(text="Add", borderwidth=0.5, highlightbackground=BACKGROUND_COLOR, font=(FONT, 15, 'normal'))
 add_button.grid(row=3, column=0, columnspan=2, sticky="EW")
-encouraging_img = ImageTk.PhotoImage(file='images/study-motivation-quotes-4.jpg')
-canvas = Canvas(width=600, height=800, bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_image(300, 400, image=encouraging_img)
-canvas.grid(row=0, column=1)
+encouraging_img = Image.open('images/foreign_lang.jpeg')
+encouraging_img = encouraging_img.resize((418, 279))
+encouraging_img = ImageTk.PhotoImage(encouraging_img)
+canvas = Canvas(width=418, height=279, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_image(209, 139.5, image=encouraging_img)
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Add word #
+word_list = 'data/word_list.json'
+def add():
+    new_word = new_word_box.get()
+    meaning = translation_box.get()
+    new_data = {new_word: meaning}
+
+
 
 
 window.mainloop()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from tkinter import *
+from PIL.ImageTk import PhotoImage
+from PIL import Image
 
 BACKGROUND_COLOR = "#FBF0B2"
 PINK = '#FFC7EA'
 BLUE = '#CAEDFF'
 PURPLE = '#D8B4F8'
-
-
 
 ################################ UI SETUP ################################
 window = Tk()
@@ -13,17 +13,23 @@ window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR)
 window.config(padx=50, pady=50)
 
-right_img = PhotoImage(file='images/right.png')
-wrong_img = PhotoImage(file='images/wrong.png')
-card_img = PhotoImage(file='images/card_front.png')
-right_button = Button(image=right_img, highlightthickness=0)
+right_img = Image.open('images/blue_tick.jpg')
+right_img = right_img.resize((100, 100))
+right_img = PhotoImage(right_img)
+wrong_img = Image.open('images/pink_cross.jpg')
+wrong_img = wrong_img.resize((100, 100))
+wrong_img = PhotoImage(wrong_img)
+
+card_img = PhotoImage(file='images/card_front.jpg')
+right_button = Button(image=right_img, highlightthickness=0, borderwidth=0, relief='flat')
 right_button.grid(row=1, column=1)
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, borderwidth=0)
 wrong_button.grid(row=1, column=2)
-card = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-card.create_image(400, 263, image=card_img)
+
+card = Canvas(width=800, height=558, bg=BACKGROUND_COLOR, highlightthickness=0)
+card.create_image(400, 279, image=card_img)
+card.create_text(400, 279, text='中文', fill='black')
 card.grid(row=0, column=1, columnspan=2)
-card.create_text(400, 263, text='中文')
 
 
 
